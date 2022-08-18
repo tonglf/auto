ac649834d49bfe117086b4f29536120140889786
tonglf <460094545@qq.com>
2022-08-18 14:17:48 +0800

    update

---
+++ ROS/tools/build/tools/CMakeFiles/readrosbag.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/tong/work/tonglf/auto/ROS/tools/devel/lib/tools/readrosbag"
+  "/home/tong/work/tonglf/auto/ROS/tools/devel/lib/tools/readrosbag.pdb"
+  "CMakeFiles/readrosbag.dir/src/readrosbag.cpp.o"
+  "CMakeFiles/readrosbag.dir/src/readrosbag.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/readrosbag.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- ROS/tools/src/tools/src/readrosbag.cpp
+++ ROS/tools/src/tools/src/readrosbag.cpp
@@ -13,38 +13,57 @@
 
 using namespace std;
 
-void handleLidarPcd(sensor_msgs::PointCloud2::ConstPtr msg);
+void handleLidarPcd(sensor_msgs::PointCloud2::ConstPtr msg, ros::Publisher &pub);
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "readrosbag");
-    ros::NodeHandle nh("readrosbag");
+    ros::NodeHandle nh("~");
 
     string bag_path;
-    string pointcloud_topic;
+    string lidar_topic;
     nh.getParam("bag_path", bag_path);
-    nh.getParam("pointcloud_topic", pointcloud_topic);
+    nh.getParam("lidar_topic", lidar_topic);
+
+    cout << "bag_path: " << bag_path << endl;
+    cout << "lidar_topic: " << lidar_topic << endl;
+
+    ros::Publisher pcd_pub = nh.advertise<sensor_msgs::PointCloud2>("pcd", 1, false);
 
     rosbag::Bag bag;
     bag.open(bag_path);
 
+    ros::Rate loop_rate(1);
+
     vector<string> topics;
-    topics.emplace_back(pointcloud_topic);
+    topics.emplace_back(lidar_topic);
     rosbag::View view(bag, rosbag::TopicQuery(topics));
 
     for (const rosbag::MessageInstance &m : view) {
-        if (pointcloud_topic == m.getTopic()) {
+        if (lidar_topic == m.getTopic()) {
             sensor_msgs::PointCloud2::ConstPtr msg = m.instantiate<sensor_msgs::PointCloud2>();
-            handleLidarPcd(msg);
+            handleLidarPcd(msg, pcd_pub);
         }
+
+        loop_rate.sleep();
     }
     bag.close();
-    
+
     return 0;
 }
 
-void handleLidarPcd(sensor_msgs::PointCloud2::ConstPtr msg) {
+void handleLidarPcd(sensor_msgs::PointCloud2::ConstPtr msg, ros::Publisher &pub) {
     pcl::PointCloud<pcl::PointXYZ>::Ptr pcd(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg(*msg, *pcd);
 
+    // ...
+    cout << "dael with pointcloud!" <<endl;
 
+    sensor_msgs::PointCloud2 ros_pcd;
+    pcl::toROSMsg(*pcd, ros_pcd);
+    
+    ros_pcd.header.frame_id = "base_link";
+    ros_pcd.header.stamp = ros::Time::now();
+
+
+    pub.publish(ros_pcd);
 }
