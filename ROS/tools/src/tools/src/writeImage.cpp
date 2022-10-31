75b900df0ff4deca04f10b6401d71f4e2ae7334c
tonglf <460094545@qq.com>
2022-10-31 11:45:04 +0800

    update ROS

--- ROS/tools/src/tools/src/writeImage.cpp
+++ ROS/tools/src/tools/src/writeImage.cpp
@@ -18,9 +18,11 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "writeImage");
     ros::NodeHandle nh("~");
 
+    bool compressed = false;
     string bag_path;
     string image_topic;
     string image_output_path;
+    nh.getParam("compressed", compressed);
     nh.getParam("bag_path", bag_path);
     nh.getParam("image_topic", image_topic);
     nh.getParam("image_output_path", image_output_path);
@@ -39,9 +41,14 @@ int main(int argc, char** argv) {
     int index = 0;
     for (const rosbag::MessageInstance &m : view) {
         if (image_topic == m.getTopic()) {
-            sensor_msgs::CompressedImage::ConstPtr camera_msg = m.instantiate<sensor_msgs::CompressedImage>();
             cv_bridge::CvImagePtr cv_ptr;
-            cv_ptr = cv_bridge::toCvCopy(camera_msg, sensor_msgs::image_encodings::BGR8);
+            if (compressed) {
+                sensor_msgs::CompressedImage::ConstPtr camera_msg = m.instantiate<sensor_msgs::CompressedImage>();
+                 cv_ptr = cv_bridge::toCvCopy(camera_msg, sensor_msgs::image_encodings::BGR8);
+            } else {
+                sensor_msgs::Image::ConstPtr camera_msg = m.instantiate<sensor_msgs::Image>();
+                cv_ptr = cv_bridge::toCvCopy(camera_msg, sensor_msgs::image_encodings::BGR8);
+            }
             cv::Mat image = cv_ptr->image.clone();
             double time = cv_ptr->header.stamp.toSec();
             cv::imwrite(image_output_path + to_string(time) + ".jpg", image);
